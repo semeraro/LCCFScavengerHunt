d0c04c221fb2b380bbc536184fffc042179191c4
hailey <haileyhickerson0@gmail.com>
2025-05-01 12:22:34 -0500

    Revert "Merge pull request #4 from haileyhickerson/ayon-lasso"
    This reverts commit 667734886bed7f6317e23679fc19827ad186c980, reversing
    changes made to b6a29757fb5e8850a45b48d7560abb3ebb9a33c1.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,24 +12,18 @@ public class GameManager : MonoBehaviour
     public GameObject image2Prefab;
     public GameObject image1CheckMark;
     public GameObject image2CheckMark;
-    public GameObject orbCheckMark;
-    public GameObject cubeCheckMark;
-    public GameObject lasso;
-    public GameObject lassoPanel;
     bool image1Tracked = false;
     bool image2Tracked = false;
     public GameObject uiBox;
     public GameObject winScreen;
     public AudioSource dingSound;
     public AudioSource completionSound;
-    public SwipeLasso swipe_lasso_script;
-    public bool lassoToggled;
     
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        lassoToggled = false;
+
     }
 
     // Update is called once per frame
@@ -54,35 +48,10 @@ public class GameManager : MonoBehaviour
        }
 
         else if (image1Tracked && image2Tracked) {
-            // winScreen.SetActive(true);
-            //uiBox.SetActive(false);
-            completionSound.Play();
-
-        }
-
-        if (swipe_lasso_script.orbCaptured)
-        {
-            orbCheckMark.SetActive(true);
-            dingSound.Play();
-        }
-
-        if (swipe_lasso_script.cubeCaptured)
-        {
-            cubeCheckMark.SetActive(true);
-            dingSound.Play();
-        }
-
-        if (swipe_lasso_script.orbCaptured && swipe_lasso_script.cubeCaptured)
-        {
-            completionSound.Play();
             winScreen.SetActive(true);
             uiBox.SetActive(false);
+            completionSound.Play();
+
         }
-    }
-    public void lassoToggle()
-    {
-        lassoToggled = !lassoToggled;
-        lassoPanel.SetActive(lassoToggled);
-        lasso.SetActive(lassoToggled);
     }
 }
