0f05c1ba0dd99c23e3ae12be2dc2faa6e2a5ed9e
hailey <haileyhickerson0@gmail.com>
2025-05-01 11:43:10 -0500

    lassoing objects

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,18 +12,24 @@ public class GameManager : MonoBehaviour
     public GameObject image2Prefab;
     public GameObject image1CheckMark;
     public GameObject image2CheckMark;
+    public GameObject orbCheckMark;
+    public GameObject cubeCheckMark;
+    public GameObject lasso;
+    public GameObject lassoPanel;
     bool image1Tracked = false;
     bool image2Tracked = false;
     public GameObject uiBox;
     public GameObject winScreen;
     public AudioSource dingSound;
     public AudioSource completionSound;
+    public SwipeLasso swipe_lasso_script;
+    public bool lassoToggled;
     
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        lassoToggled = false;
     }
 
     // Update is called once per frame
@@ -48,10 +54,35 @@ public class GameManager : MonoBehaviour
        }
 
         else if (image1Tracked && image2Tracked) {
-            winScreen.SetActive(true);
-            uiBox.SetActive(false);
+            // winScreen.SetActive(true);
+            //uiBox.SetActive(false);
             completionSound.Play();
 
         }
+
+        if (swipe_lasso_script.orbCaptured)
+        {
+            orbCheckMark.SetActive(true);
+            dingSound.Play();
+        }
+
+        if (swipe_lasso_script.cubeCaptured)
+        {
+            cubeCheckMark.SetActive(true);
+            dingSound.Play();
+        }
+
+        if (swipe_lasso_script.orbCaptured && swipe_lasso_script.cubeCaptured)
+        {
+            completionSound.Play();
+            winScreen.SetActive(true);
+            uiBox.SetActive(false);
+        }
+    }
+    public void lassoToggle()
+    {
+        lassoToggled = !lassoToggled;
+        lassoPanel.SetActive(lassoToggled);
+        lasso.SetActive(lassoToggled);
     }
 }
